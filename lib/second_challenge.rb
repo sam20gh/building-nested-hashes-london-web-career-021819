fc53ac7e6da2369d4a99466b2e5c1f5e85c28869
sam ghoreishi <sam20gh@gmail.com>
2019-02-12 23:43:13 +0000

    Automatically backed up by Learn

--- lib/second_challenge.rb
+++ lib/second_challenge.rb
@@ -16,7 +16,8 @@ def second_challenge
       },
       :hero_friends => [
         {name: "Benvolio", age: "17", attitude: "worried"},
-        {name: "Mercutio", age: "18", attitude: "hot-headed"}]
+        {name: "Mercutio", age: "18", attitude: "hot-headed"}
+        ]
       }, 
     :capulet => {
       :patriarch => {
@@ -32,7 +33,10 @@ def second_challenge
         :age => {"15"}, 
         :satus => {"alive"}
       },
-      :heroine_friends => []
+      :heroine_friends => [
+        {name: "Steven", age: "30", attitude: "confused"},
+        {name: "Nurse", age: "44", attitude: "worried"}
+        ]
       }
     
     
